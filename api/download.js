6d666b8a8b5827acf46bab89600a1686398a1dd0
RuanAlmeidaTSI <ruanalmeidaifet@gmail.com>
2024-10-17 13:56:23 -0300

    Create download.js

---
+++ api/download.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+    const filePath = path.resolve('./catalogo.pdf');
+    const fileBuffer = fs.readFileSync(filePath);
+
+    const response = new NextResponse(fileBuffer, {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="catalogo.pdf"',
+        },
+    });
+
+    return response;
+}
